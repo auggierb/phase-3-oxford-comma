33630784c0d448982871b1a2b58c3c9d0b5cd299
August <auggierutter@gmail.com>
2021-09-28 21:57:19 -0400

    commit tree de16dba4ac11c9dcfece11bbecfab49b1ab4bd69

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+if array.length == 1
+    array.join(" ")
+elsif array.length == 2
+    array.join(" and ")
+elsif array.length == 3
+nArray=array.join(", ").split
+nArray.insert(2,"and").join(" ")
+elsif array.length > 3
+    array.each do |x,y|
+        "#{x},#{y}"
+    end
 end
+end
+ 
